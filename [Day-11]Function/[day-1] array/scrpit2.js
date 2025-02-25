c37a81b9210711b73cc4eed19221f86312f90211
patel chirag <patelchiragkamlesh@gmail.com>
2025-02-25 22:29:10 +0530

    Add files via upload

---
+++ [Day-11]Function/[day-1] array/scrpit2.js
@@ -0,0 +1,16 @@
+// wap to print O on even number 1 on odd number
+
+let num = [1,2,3,4,5,6,7,8,67,83]
+
+
+for(let i=0; i<=num.length ; i++)
+{
+         if(num[i]%2==0)
+         {
+            document.write("0");
+         }
+         else
+         {
+            document.write("1");
+         }
+}
