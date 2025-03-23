07eb194c8dd183d44186ee5407dbcb555f66ed5b
patel chirag <patelchiragkamlesh@gmail.com>
2025-03-23 12:28:07 +0530

    Add files via upload

---
+++ dom/background color generator/scrpit.js
@@ -0,0 +1,11 @@
+
+const body = document.querySelector('body');
+const colorcopybtn = document.querySelector('#color');
+const btn = document.querySelector('.btn');
+
+btn.addEventListener('click', () => {
+    let color = Math.floor(Math.random() * 16777216).toString(16);
+    body.style.backgroundColor = "#" + color;
+    colorcopybtn.innerHTML = "#" + color;
+     navigator.clipboard.writeText( "#" + color)   
+});
